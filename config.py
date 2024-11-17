ffb07573fc7386347f8c855c8a847a1e2f8c8909
Bisnu Ray <79582318+bisnuray@users.noreply.github.com>
2024-11-17 22:25:22 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "7361762"  # Replace this API ID with your actual API ID
+API_HASH = "fffa021a2ce0ed383eac2f53b49af2e4"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "6687090845:AAEsz8GrAWa53MUa63LJ9EhQhuOuXJkl7Fc"  # Replace this BOT_TOKEN
