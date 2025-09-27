6c169946464dccabe9e040607182e055fd95939c
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-09-27 07:27:37 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
-
-# Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "1234567890:XXXXXXXXXXXXX"  # Replace this BOT_TOKEN
