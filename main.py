f012f1cb10ef94136c0cd040d164a57fa9dc09ae
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-09-27 09:12:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from modules.info import info_command
 from modules.donate import donate_command
 from modules.fwd import handle_forwarded_message
 from modules.username import username_command
+from modules.callback import admin_button_handler, owner_button_handler
 from shared.chatinfo import handle_message
 
 LOGGER.info("QuickInfoBot Started Successfully 💥")
