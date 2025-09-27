98ca725edeea1de2fc81f03fee1d6dea378183cb
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-09-27 09:13:30 +0600

    Create callback.py

---
+++ modules/callback.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, filters
+from pyrogram.types import ReplyKeyboardMarkup
+from utils import LOGGER
+from miscs.adbtn import admin_buttons
+from miscs.mybtn import my_buttons
+from bot import bot
+
+@bot.on_message(filters.text & filters.regex(r"^👥 Admins Chat$"))
+async def admin_button_handler(bot: Client, message):
+    LOGGER.info("Admins Chat button clicked")
+    await message.reply_text(
+        "**🛡️ Channels and Groups Where You Are Admin**\n\n"
+        "🔧 **How to Use?**\n"
+        "1️⃣ Click the buttons below to share a channel or group where you have admin privileges.\n"
+        "2️⃣ Receive the unique ID instantly.\n\n"
+        "> 🛠 Made with ❤️ By @TheSmartDev",
+        reply_markup=admin_buttons
+    )
+
+@bot.on_message(filters.text & filters.regex(r"^👑 Owner Chat$"))
+async def owner_button_handler(bot: Client, message):
+    LOGGER.info("Owner Chat button clicked")
+    await message.reply_text(
+        "**📚 Your Channels and Groups**\n\n"
+        "🔧 **How to Use?**\n"
+        "1️⃣ Click the buttons below to share your channel or group.\n"
+        "2️⃣ Receive the unique ID instantly.\n\n"
+        "> 🛠 Made with ❤️ By @TheSmartDev",
+        reply_markup=my_buttons
+    )
