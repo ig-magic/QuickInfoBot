6d62d406d0892336e9d06bde894358b448dbdd04
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-09-27 07:34:33 +0600

    Create start.py

---
+++ core/start.py
@@ -0,0 +1,23 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+from pyrogram import Client, filters
+from utils import LOGGER
+from miscs.startbtn import menu_buttons
+from bot import bot
+from config import COMMAND_PREFIX
+
+@bot.on_message(filters.command("start", prefixes=COMMAND_PREFIX.split("|")))
+async def start(bot: Client, message):
+    LOGGER.info("Start command received")
+    await message.reply_text(
+        "**👋 Welcome to Chat ID Finder Bot!** 🆔\n\n"
+        "**✅ Fetch Any Chat ID Instantly!**\n\n"
+        "🔧 **How to Use?**\n"
+        "1️⃣ Click the buttons below to share a chat or user.\n"
+        "2️⃣ Receive the unique ID instantly.\n\n"
+        "💎 **Features:**\n"
+        "- Supports users, bots, private/public groups & channels\n"
+        "- Fast and reliable\n\n"
+        "> 🛠 Made with ❤️ By @TheSmartDev",
+        reply_markup=menu_buttons
+    )
